1868bef6cb994de5ff481049087936767d677795
aein <mananniaein1234@gmail.com>
2024-11-24 14:22:56 +0330

    set new config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Container, Grid2 } from "@mui/material";
+
 export default function App() {
   return (
-    <div>
-      <span>my materail ul Components</span>
-    </div>
+    <Container>
+      <Grid2 container>
+        <Grid2
+          size={{ xs: 12, sm: 12, md: 4, lg: 3 }}
+          style={{ backgroundColor: "blue" }}
+        >
+          ABC
+        </Grid2>
+        <Grid2
+          size={{ xs: 12, sm: 12, md: 8, lg: 9 }}
+          style={{ backgroundColor: "red" }}
+        >
+          DEF
+        </Grid2>
+      </Grid2>
+    </Container>
   );
 }
